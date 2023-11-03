b90acf23593f6bae7592294a071508aabbe859b1
evaniewares <chisopsyelera@gmail.com>
2023-11-03 11:21:47 +0000

    task 8

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,16 +1,4 @@
 #!/usr/bin/env ruby
-if ARGV.length != 1
-	puts "Usage: #{$PROGRAM_NAME} <log_file>"
-	exit(1)
-end
-  
-log_file = ARGV[0]
-  
-File.open(log_file, 'r').each_line do |line|
-	if line =~ /\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/
-		sender = $1
-		receiver = $2
-		flags = $3
-		puts "#{sender},#{receiver},#{flags}"
-	end
-end  
+# Displays the sender and receiver's phone number as well as the flags
+# that were used
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
