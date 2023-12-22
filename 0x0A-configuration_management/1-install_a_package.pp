911f8621ce4130b6e89923c9bcc64eff5ff99d97
evaniewares <chisopsyelera@gmail.com>
2023-12-22 20:27:31 +0200

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,13 +1,7 @@
+#!/usr/bin/pup
 # Install flask from pip3.
 
-# Update system
-exec { 'apt update':
-  command => '/usr/bin/apt-get update'
-}
-
-# Install flask
 package { 'flask':
   ensure   => '2.1.0',
-  provider => 'pip3',
-  require  => Exec['apt update']
+  provider => 'pip3'
 }
