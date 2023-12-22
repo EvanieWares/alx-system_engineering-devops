fd3012d934a2b54b01bb35aa7a0c66fcf34ff81c
evaniewares <chisopsyelera@gmail.com>
2023-12-22 21:04:43 +0200

    1. Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,26 @@
-#!/usr/bin/pup
 # Install flask from pip3.
 
+# Install Python (3.8.10)
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
+# Install pip
+package { 'python3-pip':
+  ensure  => 'present',
+  require => Package['python3.8'],
+}
+
+# Install Werkzeug (2.1.1)
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3',
+  require  => Package['python3-pip'],
+}
+
+# Install flask (2.1.0)
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
