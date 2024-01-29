c4884aa9246a0d6744da3b5c1444d62e283c6ea7
evaniewares <chisopsyelera@gmail.com>
2024-01-29 19:07:45 +0200

    api - updated

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -7,28 +7,14 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: 0-gather_data_from_an_API.py <employee ID>")
-        exit(1)
-    try:
-        employee_id = int(sys.argv[1])
-    except ValueError as e:
-        print('Error:', e)
-        exit(1)
-
+    user_id = int(sys.argv[1])
     base_url = "https://jsonplaceholder.typicode.com"
-    todo_url = f"{base_url}/todos?userId={employee_id}"
-    user_url = f"{base_url}/users/{employee_id}"
-    try:
-        todos_response = requests.get(todo_url)
-        users_response = requests.get(user_url)
-        todos_response.raise_for_status()
-        users_response.raise_for_status()
-    except requests.exceptions.RequestException as e:
-        print("Error", e)
-        exit(1)
+    todo_url = f"{base_url}/todos?userId={user_id}"
+    user_url = f"{base_url}/users/{user_id}"
+    todos_response = requests.get(todo_url)
+    users_response = requests.get(user_url)
 
-    csv_file = f'{employee_id}.csv'
+    csv_file = f'{user_id}.csv'
     name = users_response.json().get('name')
     todos = todos_response.json()
 
@@ -36,5 +22,5 @@ if __name__ == "__main__":
         writer = csv.writer(f, quoting=csv.QUOTE_ALL)
         for todo in todos:
             writer.writerow(
-                (employee_id, name, todo.get('completed'), todo.get('title'))
+                (user_id, name, todo.get('completed'), todo.get('title'))
             )
